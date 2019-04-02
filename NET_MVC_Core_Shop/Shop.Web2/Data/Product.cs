1c92c683214724bdaa43139a604a33179a6ed496
isaiyepez <isaiyepez@gmail.com>
2019-04-01 23:43:16 -0600

    Progress with backend

---
+++ NET_MVC_Core_Shop/Shop.Web2/Data/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web2.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImageUrl { get; set; }
+
+        [Display(Name = "Last Purchase")]
+        public DateTime LastPurchase { get; set; }
+
+        [Display(Name = "Last Sale")]
+        public DateTime LastSale { get; set; }
+
+        [Display(Name = "Is Availabe?")]
+        public bool IsAvailabe { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+    }
+}
