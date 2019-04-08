e4efde830dd8d62941f522191ddde5c06c5ec9a0
isaiyepez <isaiyepez@gmail.com>
2019-04-08 18:58:30 -0500

    API created

---
+++ NET_MVC_Core_Shop/Shop.Web2/Controllers/API/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Web2.Data;
+
+namespace Shop.Web2.Controllers.API
+{
+    [Route("api/[Controller]")]
+
+
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+
+    }
+}
